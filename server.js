b84e2cb69086b35006c4131259fd966d7fb6e343
sebhoe <sebhoe@freenet.de>
2016-06-05 12:58:38 +0000

    deploy ready (work's on nitrous)

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/app/assets'));
+
+// make express find the node_modules
+app.use(express.static(__dirname + '/node_modules'));
+
+// make express find the systemjs.config.js
+app.use(express.static(__dirname + '/'));
+
+
+// set the home page route
+app.get('/', function(req, res) {
+
+    // ejs render automatically looks in the views folder
+    res.render('index');
+});
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
